3816687ea4355a123cf12f90af2fe423b10c0a2b
ayorishy <qw1139074074@qq.com>
2024-10-22 14:40:04 +0800

    Update __init__.py

--- python/kcard/__init__.py
+++ python/kcard/__init__.py
@@ -1,13 +1,9 @@
 from .model.base import *
 from .model.reply import *
 from .model.logo import *
-from .model.tips import *
-from .model.welcome import *
 
 __all__ = [
     'base',
     'reply_status', 'reply_tips', 'reply_status_code',
     'logo_text'
-    'tips_text'
-    'welcome_text'
 ]
