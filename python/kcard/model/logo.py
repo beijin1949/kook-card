fe2b2ac6625eabb29971662afac26c84fe99e9e5
ayorishy <qw1139074074@qq.com>
2024-10-22 14:24:00 +0800

    新增文品牌卡片

--- python/kcard/model/logo.py
+++ python/kcard/model/logo.py
@@ -17,7 +17,6 @@ def logo_img(logo_url: str, content: str, color: str = "#409EFF") -> CardMessage
     c.append(Module.Section(content))
     return CardMessage(c)
 
-
 def logo_mini(logo_url: str, content: str, logo_text: str = "", color: str = "#409EFF") -> CardMessage:
     """
     品牌卡片，顶部
@@ -31,4 +30,34 @@ def logo_mini(logo_url: str, content: str, logo_text: str = "", color: str = "#4
     c.append(Module.Context(Element.Image(src=logo_url), Element.Text(f"**{logo_text}**")))
     c.append(Module.Divider())
     c.append(Module.Section(content))
+    return CardMessage(c)
+
+def logo_tips(logo_url: str, content: str, color: str = "#409EFF") -> CardMessage:
+    """
+    小提示卡片，左边展示，在左边展示卡片图片
+    :param logo_url: 图片URL链接
+    :param content::内容
+    :param color: 卡片颜色
+    :return: CardMessage
+    """
+    c = Card(color=color)
+    c.append(Module.Context(Element.Image(src=logo_url), Element.Text(content),))
+    return CardMessage(c)
+
+def logo_welcome(logo_url: str, content: str, desc1: str, desc2: str, bg_url: str, color: str = "#409EFF") -> CardMessage:
+    """
+    进出提示卡片，中间展示，居中展示卡片图片 (不建议超过 732X193 尺寸)
+    :param logo_url: 图片URL链接
+    :param content: 内容
+    :param desc1: 顶部描述
+    :param desc2: 底部描述
+    :param bg_url: 居中图片
+    :param color: 卡片颜色
+    :return: CardMessage
+    """
+    c = Card(color=color)
+    c.append(Module.Section(Element.Text(content)))
+    c.append(Module.Context(Element.Image(logo_url), Element.Text(desc1)))
+    c.append(Module.Container(Element.Image(bg_url)))
+    c.append(Module.Section(Element.Text(desc2)))
     return CardMessage(c)
